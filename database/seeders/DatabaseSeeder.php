0a1cd57fee9f6e623a03f513f8381d3f337a2b36
Mustapha <mhammouny@rc2k.fr>
2024-01-10 23:42:39 +0100

    create seeders & factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,44 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Choice;
+use App\Models\Question;
+use App\Models\Quiz;
+use App\Models\Tenant;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        for ($i = 1; $i <= 10; $i++) {
+            $subdomain = "user{$i}";
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $tenant = Tenant::create(['id' => $subdomain]);
+            $tenant->domains()->create(['domain' => "{$subdomain}.localhost"]);
+
+            $tenant->run(function () use ($subdomain) {
+                User::factory()->create([
+                    'name' => $subdomain,
+                    'email' => "{$subdomain}@quiz.com",
+                ]);
+
+                Quiz::factory()
+                    ->count(10)
+                    ->has(
+                        Question::factory()
+                            ->count(10)
+                            ->has(
+                                Choice::factory()
+                                    ->count(10)
+                                    ->sequence(fn (Sequence $sequence) => ['order' => ($sequence->index + 1) % 10])
+                            )
+                    )
+                    ->create();
+            });
+        }
     }
 }
